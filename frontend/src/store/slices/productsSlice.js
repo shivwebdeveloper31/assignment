73ca059f00c4b5021aa81d532d8e2409b7363fe9
shivwebdeveloper31 <shivwebdeveloper31@gmail.com>
2026-05-08 23:21:57 +0530

    frontend-deploy

--- frontend/src/store/slices/productsSlice.js
+++ frontend/src/store/slices/productsSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const API = process.env.REACT_APP_API_URL || '/api';
+const API = process.env.REACT_APP_API_URL;;
 
 export const fetchProducts = createAsyncThunk(
   'products/fetchProducts',
